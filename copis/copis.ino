4c566d113eaf45920a986229944c88c590473d2f
TheGrimReaper13 <58264881+TheGrimReaper13@users.noreply.github.com>
2024-05-25 12:27:39 +0200

    Update copis.ino

--- copis/copis.ino
+++ copis/copis.ino
@@ -2,55 +2,66 @@
   - ignores blades touching for now but doesn't affect other behaviour, no whip-over yet also doesn't account for contact through the blades
   - as micros loops/flows-over after 70min device needs to be restarted (maybe add signal and something to handle a reset in software when it overflows)
 
-   W_PIN: digital output
-   L_PIN: digital input
-   C_PIN: didital input 
+  W_PIN: digital output
+  L_PIN: digital input
+  C_PIN: didital input 
+
+  - if we touch ourself signal with yellow led, FIE rulebook seems to imply this lamp is directly linked to that state, so it doesn't stay on (maybe just keep it on until reset?)
+
 */
 
 # define CONTROL_USED
 # undef CONTROL_USED
 
-const unsigned long DEPRESS_TIME = 500;     // in us, 0.5ms
-const unsigned long LOCKOUT_TIME = 170000;  // in us, 170ms
-const long RESET_TIME = 3000; // in ms, 3s
+const unsigned long   DEPRESS_TIME      = 300;     // in us, 0.5ms
+const unsigned long   LOCKOUT_TIME      = 170000;  // in us, 170ms
+const unsigned long   ERROR_TIME        = 3000;    // in us, 3ms
+const long            RESET_TIME        = 3000;    // in ms, 3s
 
-const uint8_t SOUND_SIGNAL_PIN = 9;
-const uint8_t HOLD_PIN = 2; // toggle switch, probably better to make this a momentary switch and a software toggle
+const uint8_t         SOUND_SIGNAL_PIN  = 9;       // produces square wave and is connected to speaker 
+const uint8_t         HOLD_PIN          = 2;       // toggle switch, probably better to make this a momentary switch and a software toggle
 
-# define GREEN_WEAPON_PIN 3
-# define RED_WEAPON_PIN 4
-# define GREEN_LAME_PIN 5
-# define RED_LAME_PIN 6
-# define GREEN_CONTROL_PIN 7
-# define RED_CONTROL_PIN 8
-# define GREEN_SIGNAL_PIN 10
-# define RED_SIGNAL_PIN 11
-# define GREEN_ERROR_PIN 12
-# define RED_ERROR_PIN 13
+# define GREEN_WEAPON_PIN   3
+# define RED_WEAPON_PIN     4
+# define GREEN_LAME_PIN     5
+# define RED_LAME_PIN       6
+# define GREEN_CONTROL_PIN  7
+# define RED_CONTROL_PIN    8
+# define GREEN_SIGNAL_PIN   10
+# define RED_SIGNAL_PIN     11
+# define GREEN_ERROR_PIN    12
+# define RED_ERROR_PIN      13
+# define GREEN_SELF_HIT_PIN A0
+# define RED_SELF_HIT_PIN   A1
 
 struct Fencer {
-  unsigned long depressed_time;
-  unsigned long lockout_time;
-  bool hit;
-  bool error; // signals unnormal change in control circuit
+  unsigned long depressed_time; // time when contact was made
+  unsigned long lockout_time;   // time when valid hit was made
+  unsigned long error_time;     // time when break in control circuit was detected
+  bool          hit;            // we need to be able to remember that a hit was made
+  bool          error;          // signals break in control circuit
 
-  const uint8_t W_PIN;  // C line
-  const uint8_t L_PIN;  // A line
-  const uint8_t C_PIN;  // B line
-  const uint8_t SIGNAL_PIN;
-  const uint8_t ERROR_PIN;
+  const uint8_t W_PIN;          // C line
+  const uint8_t L_PIN;          // A line
+  const uint8_t C_PIN;          // B line
+  const uint8_t SIGNAL_PIN;     // red/green
+  const uint8_t ERROR_PIN;      // white
+  const uint8_t SELF_HIT_PIN;   // yellow
 };
 
-Fencer green =  { 0, 0, false, false, GREEN_WEAPON_PIN, GREEN_LAME_PIN, GREEN_CONTROL_PIN, GREEN_SIGNAL_PIN, GREEN_ERROR_PIN };
-Fencer red =    { 0, 0, false, false, RED_WEAPON_PIN, RED_LAME_PIN, RED_CONTROL_PIN, RED_SIGNAL_PIN, RED_ERROR_PIN };
+Fencer green =  { 0, 0, 0, false, false, GREEN_WEAPON_PIN, GREEN_LAME_PIN, GREEN_CONTROL_PIN, GREEN_SIGNAL_PIN, GREEN_ERROR_PIN, GREEN_SELF_HIT_PIN };
+Fencer red =    { 0, 0, 0, false, false, RED_WEAPON_PIN, RED_LAME_PIN, RED_CONTROL_PIN, RED_SIGNAL_PIN, RED_ERROR_PIN, RED_SELF_HIT_PIN };
 
 void reset(Fencer *p) {
   // W_PIN is always LOW after exiting checkHit
   // turn off signal lights
   digitalWrite(p->SIGNAL_PIN, LOW);
+  digitalWrite(p->ERROR_PIN, LOW);
+  digitalWrite(p->SELF_HIT_PIN, LOW);
 
   p->depressed_time = 0;
   p->lockout_time = 0;
+  p->error_time = 0;
   p->hit = false;
   p->error = false;
 } // end reset
@@ -69,6 +80,10 @@ void signalTone() {
 // checks if attacker made valid hit
 void checkHit(Fencer *att, Fencer *def, unsigned long now) {
   digitalWrite(att->W_PIN, HIGH);
+  // check for self hit, we don't need to interrupt anything, just signal that we hit ourself (we probably want to make this so it stays on until reset is called)
+  if (digitalRead(att->L_PIN) == HIGH) {
+    digitalWrite(att->SELF_HIT_PIN, HIGH);
+  }
 # ifdef CONTROL_USED
   // check control circuit, if we can't read a signal on the control circuit 
   if (digitalRead(att->C_PIN) == LOW) {
